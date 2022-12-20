5f9608ad49608c26cf8a9bb36edd33200c076f1f
arafa0 <121126457+arafa0@users.noreply.github.com>
2022-12-20 20:47:13 +0200

    Add files via upload

---
+++ prime number 00/prime number 00/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number_00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("pls Entert the frist number ");
+            int start = int .Parse(Console.ReadLine());
+
+            Console.WriteLine("pls Entert the frist number ");
+            int  end  = int.Parse(Console.ReadLine());
+
+            int i, j;
+            Console.WriteLine("prime no between  start to end  ");
+            for (i = start ; i <= end ; i++)
+            {
+                int v = 0;
+                for (j = 1; j <= i; j++)
+                {
+
+                    if (i % j == 0)
+                    { v = v + 1; }
+                }
+
+                if (v <= 2)
+                { Console.WriteLine(i); }
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
